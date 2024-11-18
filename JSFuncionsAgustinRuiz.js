44527629bea3d8126e36c48bb755412d14bc1410
Agustín <agustinruiz@institutvidreres.cat>
2024-11-18 11:39:40 +0100

    Exercici 1 i coses

--- JSFuncionsAgustinRuiz.js
+++ JSFuncionsAgustinRuiz.js
@@ -1,1 +1,20 @@
-Hola
+//exercici 1 -
+function exercici1(nom = "Agustín", cognoms = "Ruiz Sosa") {
+    console.log(nom + " " + cognoms)
+}
+exercici1()
+
+//exercici 2 - Passa un valor per paràmetre i mostri de quin tipus és.
+//exercici 3 - Mostri la data actual
+//exercici 4 - Declara una variable dins d’una funció i mostra el seu valor fora d’aquesta. Què passa? Perquè?
+//exercici 5 - Passa’t un String, retorni el número de vocals que té
+//exercici 6 - Passa 3 enters com a paràmetres (el tercer ha de tenir un valor per defecte) i retorni la suma dels tres.
+//exercici 7 - Ha de cridar la funció de l’exercici 3 i retornar el resultat de la funció de l’exercici 6.
+//exercici 8 - Passa’t dos enters, retornar true si un d’ells és igual a 30 o si la seva suma és igual a 30.
+//exercici 9 - Repeteix l’exercici anterior però com a funció anònima.
+//exercici 10 - Passa’t dos valors, retorni cert o falç si el valor i el tipus de dades són el mateix.
+//exercici 11 - Passa’t un valor, realitzi l’arrel quadrada i retorni el valor amb dos decimals.
+////exercici 12 - Passa’t un String, el retorni en minúscules.
+//exercici 13 - Passa’t dos valors, retorni el que no és null.
+//exercici 14 - Defineix dos funcions:
+//exercici 15 - Explica què fa la següent funció i fes-ne un cas de prova:
